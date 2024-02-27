6b7590dad2a1dc6bd32653132699a3193d06b9c8
yourstamal <161326121+yourstamal@users.noreply.github.com>
2024-02-27 12:14:40 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,45 @@
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
+let btn5 = document.querySelector("#btn5")
+let btn6 = document.querySelector("#btn6")
+let btn7 = document.querySelector("#btn7")
+let btn8 = document.querySelector("#btn8")
+let btn9 = document.querySelector("#btn9")
+let btn10 = document.querySelector("#btn10")
+let btn11 = document.querySelector("#btn11")
+
+btn1.onclick = () => {
+alert (" Temparature is :- 45°C \n Rain percentage :- 90% ")
+}
+btn2.onclick = () => {
+alert (" Temparature is :- 40°C \n Rain percentage :- 80% ")
+}
+btn3.onclick = () => {
+alert (" Temparature is :- 35°C \n Rain percentage :- 70% ")
+}
+btn4.onclick = () => {
+alert (" Temparature is :- 30°C \n Rain percentage :- 60% ")
+}
+btn5.onclick = () => {
+alert (" Temparature is :- 20°C \n Rain percentage :- 50% ")
+}
+btn6.onclick = () => {
+alert (" Temparature is :- 25°C \n Rain percentage :- 40% ")
+}
+btn7.onclick = () => {
+alert (" Temparature is :- 35°C \n Rain percentage :- 30% ")
+}
+btn8.onclick = () => {
+alert (" Temparature is :- 30°C \n Rain percentage :- 20% ")
+}
+btn9.onclick = () => {
+alert (" Temparature is :- 40°C \n Rain percentage :- 10% ")
+}
+btn10.onclick = () => {
+alert (" Temparature is :- 10°C \n Rain percentage :- 0% ")
+}
+btn11.onclick = () => {
+alert (" Temparature is :- 20°C \n Rain percentage :- 100% ")
+}
